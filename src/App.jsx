3d0304e546cd8e7537ad97462bda57c499460490
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-02 11:14:20 -0500

    Simple example with fixed cards and ID, no randomized shuffle

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import './App.css'
+import { useState } from 'react'
+import Gameboard from './Gameboard';
 
 function App() {
+
+  const [score, setScore] = useState(0);
+  const [hiScore, setHiScore] = useState(0);
+
+
   return (
-    <div>Hello World</div>
+    <>
+    <div className='header'>
+      <h1>Memory Card Game</h1>
+      <div>Score: {score}</div>
+      <div>High-Score: {hiScore}</div>
+    </div>
+    <Gameboard score={score} setScore={setScore} hiScore={hiScore} setHiScore={setHiScore}/>
+    </>
   )
 }
 
